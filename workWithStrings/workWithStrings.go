fcdc3fdda67ffb8deb5a2f5d3d26f8d3bcdd6bdc
Lada Bratsykhina <spirirted.away@gmail.com>
2020-04-20 11:31:58 +0300

    update workWithStrings

--- workWithStrings/workWithStrings.go
+++ workWithStrings/workWithStrings.go
@@ -1,6 +1,6 @@
-// analyze entered string for order of opening and closing symbols
+// analyze order of opening and closing symbols in string
 // colorize opening and closing symbols in string
-// find the longest string part in alphabetical order of english alphabet
+// find the longest substring in alphabetical order of english alphabet
 
 package main
 
@@ -12,17 +12,14 @@ import (
 	"github.com/fatih/color"
 )
 
-// function analize(stroka, openSymbol, closeSymbol string)
-// return string with results of opening and closing symbols order's analysis in stroka string
-// analysis is performed by putting opening brackets into and out of stack array
+// analyze order of opening and closing symbols in stroka string
 // parameters:
 //   stroka - string for analysis
 //   openSymbol - opening symbol for analysis
 //   closeSymbol - closing symbol for analysis
-
 func analyze(stroka, openSymbol, closeSymbol string) {
 
-	var counter int = 0 // counter for opening symbols from stroka string
+	var counter int = 0 // counter for opening symbols
 	var result string   // result with analysis of opening and closing symbols order
 
 	for _, char := range stroka {
@@ -52,7 +49,7 @@ AnalysisResult:
 	fmt.Println("Order of", openSymbol, "and", closeSymbol, "in string is", result, "!")
 }
 
-// print symbol with character ANCII code with colour
+// print coloured symbol with character ANCII code
 func printColoredCharacter(character rune, colour color.Attribute) {
 	color.Set(colour)
 	fmt.Print(string(character))
@@ -67,7 +64,7 @@ func printColoredCharacter(character rune, colour color.Attribute) {
 //   closeSymbol - closing symbol for analysis
 func colorize(stroka, openSymbol, closeSymbol string) {
 
-	var counter int = 0 // counter for opening symbols from stroka string
+	var counter int = 0 // counter for opening symbols
 	var index int = 0   // index for non-pair closing symbols
 
 	var colors []color.Attribute // array with colors for opening and closing symbols
@@ -101,33 +98,36 @@ func colorize(stroka, openSymbol, closeSymbol string) {
 				fmt.Print(string(char))
 			}
 		}
-
 	}
 }
 
-// check is character ANCII code corresponds to ANCII code of upper case english letter or not
+// check if ANCII code corresponds to ANCII code of upper case english letter
+// parameter: character - ANCII code
 func isUpperCase(character rune) bool {
 	return character >= 65 && character <= 90
 }
 
-// check is character ANCII code corresponds to ANCII code of lower case english letter or not
+// check if ANCII code corresponds to ANCII code of lower case english letter
+// parameter: character - ANCII code
 func isLowerCase(character rune) bool {
 	return character >= 97 && character <= 122
 }
 
-// return ANCII code of upper case english letter corresponding to
-// ANCII code of character lower case english letter
+// return ANCII code of upper case english letter corresponded to
+// ANCII code of lower case english letter
+// parameter: character - ANCII code
 func changedToUpperCase(character rune) rune {
 	return character - 32
 }
 
-// find the longest string part in alphabetical order of english alphabet
-// upper and lower case of characters is ignored
+// find the longest substring in alphabetical order of english alphabet
+// upper- or lower- case is ignored
+// parameter: stroka - string for analysis
 func findLongestAlphabeticalPart(stroka string) {
 
 	var index int = 0                      // index for stroka characters
 	var strArray []string                  // array with characters from stroka string
-	var runeArray []rune                   // array with ANCII codes of characters from stroka string
+	var runeArray []rune                   // array with characters ANCII codes from stroka string
 	var strTemp1, strTemp2 string = "", "" // variables for composing and finding the longest substring in alphabetical order
 	var temp1, temp2 rune = 0, 0           // variables for comparing ANCII codes from runeArray
 
@@ -147,7 +147,7 @@ func findLongestAlphabeticalPart(stroka string) {
 		if isUpperCase(temp1) {
 			if index == 0 {
 				strTemp1 = strArray[0]
-			} else if temp1 > temp2 {
+			} else if temp1 >= temp2 {
 				strTemp1 += strArray[index]
 			}
 			if len(strTemp2) < len(strTemp1) {
@@ -161,14 +161,11 @@ func findLongestAlphabeticalPart(stroka string) {
 		}
 		index++
 	}
-
 	if len(strTemp2) < len(strTemp1) {
 		strTemp2 = strTemp1
 	}
-
-	fmt.Println("The longest string part in alphabetical order is: ", strTemp2,
+	fmt.Println("The longest substring in alphabetical order is: ", strTemp2,
 		". It's length = ", len(strTemp2))
-
 }
 
 // program execution
@@ -195,13 +192,13 @@ func main() {
 
 	fmt.Println("---")
 
-	// colorize opening and closing symbols in the string
+	// colorize opening and closing symbols in the str
 	colorize(str, opening, closing)
 
-	// analyze opening and closing symbols order in entered string
+	// analyze opening and closing symbols order in str
 	analyze(str, opening, closing)
 
-	// find the longest string part in alphabetical order of english alphabet
+	// find the longest substring in alphabetical order of english alphabet in str
 	// upper and lower case of characters is ignored
 	findLongestAlphabeticalPart(str)
 

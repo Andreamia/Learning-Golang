7d2e3f96a1b73f3af42ff2b9615bf20373cabf63
Lada Bratsykhina <spirirted.away@gmail.com>
2020-04-24 11:24:12 +0300

    weatherMonitor updated, sound added

--- weatherMonitor/weatherMonitor.go
+++ weatherMonitor/weatherMonitor.go
@@ -1,9 +1,26 @@
+// Don't forget to do before run the program:
+// 	- install "github.com/faiface/beep" package			: 	go get -u github.com/faiface/beep
+// 	- install "github.com/faiface/beep/wav3" package	: 	go get -u github.com/faiface/beep/wav
+// 	- install "github.com/faiface/beep/speaker" package	: 	go get -u github.com/faiface/beep/speaker
+//
+// This program:
+// 	- get current weather parameters (temperature, pressure, humiduty) for entered city
+// from openweathermap.org
+// 	- notify user by message and sound alarm if these weather parameters have changed in comparison
+// with previous ones
+//
+// Please, take into account that weather data in "openweathermap.org" is updated
+// no more than one time every 10 minutes.
+// So to see changes in weather parameters it's recommended to making http request to the API
+// no more than one time every 10 minutes for one location
+
 package main
 
 import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math"
 	"net/http"
@@ -11,6 +28,10 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/wav"
 )
 
 // constants for getting access to openweather API
@@ -45,7 +66,6 @@ func currentWeather(cityName string) (CurrentWeather, error) {
 
 	// compile http request and get http response
 	url := "http://" + apiURL + "/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey
-
 	resp, err := client.Get(url)
 	if err != nil {
 		panic(err)
@@ -64,10 +84,18 @@ func currentWeather(cityName string) (CurrentWeather, error) {
 		panic(err)
 	}
 
+	// notify if city with cityName wasn't found
+	if currentWeather.CityID == 0 && currentWeather.CityName == "" {
+		fmt.Println("-----")
+		fmt.Printf("There is NO CITY with name = %s!\n", cityName)
+		os.Exit(3)
+	}
+
 	return currentWeather, err
 }
 
-// compare current weather parametrs(temperature, pressure, humiduty) with previous ones
+// compare current weather parametrs(temperature, pressure, humiduty) from weatherCurrent
+// with previous ones from weatherPrevious
 func weatherChanged(weatherPrevious, weatherCurrent CurrentWeather) (bool, bool, bool) {
 
 	var isTemperatureChanged bool = false
@@ -86,15 +114,11 @@ func weatherChanged(weatherPrevious, weatherCurrent CurrentWeather) (bool, bool,
 	return isTemperatureChanged, isPressureChanged, isHumidityChanged
 }
 
-// print current weather parameters (temperature, pressure, humiduty)
-// notify user if parameter's values have changed: message + sound
-func printCurrentWeather(cityName string, weather CurrentWeather, isTemperatureChanged, isPressureChanged, isHumidityChanged bool) {
+// show current weather parameters (temperature, pressure, humiduty)
+// notify user if parameter's values have changed: message + sound alarm
+func showCurrentWeather(cityName string, weather CurrentWeather, isTemperatureChanged, isPressureChanged, isHumidityChanged bool) {
 
 	fmt.Println("-----")
-	if weather.CityID == 0 && weather.CityName == "" {
-		fmt.Printf("There is NO CITY with name = %s!\n", cityName)
-		os.Exit(3)
-	}
 	fmt.Printf("Current weather in %s:\n", cityName)
 
 	if isTemperatureChanged == true {
@@ -114,10 +138,13 @@ func printCurrentWeather(cityName string, weather CurrentWeather, isTemperatureC
 	} else {
 		fmt.Printf("  Humidity = %3d %%\n", weather.Weather.Humidity)
 	}
+	if isTemperatureChanged == true || isPressureChanged == true || isHumidityChanged == true {
+		playAlarm()
+	}
 }
 
 // delay before next weather request
-func askForDelay() error {
+func delayBeforeNextRequest() error {
 
 	var delay int
 	var strDelay string
@@ -134,8 +161,75 @@ func askForDelay() error {
 	return err
 }
 
+// download file from url to "alarm.wav" file in current directory
+func downloadWAVFileWithAlarm() error {
+
+	var url string = "http://stuffnewspaper.com/sounds/MISC/bell.wav"
+	var filePath string
+
+	filePath, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	filePath += "\\alarm.wav"
+
+	// get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// create the file
+	out, err := os.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+	// write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+// play "alarm.wav"
+func playAlarm() {
+
+	// downaload file with alarm sound
+	err := downloadWAVFileWithAlarm()
+	if err != nil {
+		panic(err)
+	}
+
+	// open file, so that we can decode and play it
+	file, err := os.Open("alarm.wav")
+	if err != nil {
+		panic(err)
+	}
+
+	// decode file with wav.Decode to streamer
+	streamer, format, err := wav.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+	defer streamer.Close()
+
+	// initialize the speaker
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	// play the streamer
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
+
+}
+
 // program execution
 func main() {
+
 	var weather1, weather2 CurrentWeather
 	var city string
 	var isTempChanged, isPresChanged, isHumChanged bool = false, false, false
@@ -152,19 +246,21 @@ func main() {
 MonitorWeather:
 	for true {
 		// user decides if he wants to monitor weather in city
-		fmt.Println("")
+
+		fmt.Print("\a")
 		fmt.Println("Enter 1 or 2 depending on your choice:")
 		fmt.Printf("1. Monitor weather in %s\n", city)
 		fmt.Printf("2. Exit the program\n")
 		fmt.Scanln(&response)
 		response = strings.TrimSpace(response)
+		fmt.Print("\a")
 
 		switch response {
 		// monitor weather in city
 		case "1":
 			{
 				if monitorCounter != 0 {
-					err = askForDelay()
+					err = delayBeforeNextRequest()
 					if err != nil {
 						panic(err)
 					}
@@ -178,7 +274,7 @@ MonitorWeather:
 				if monitorCounter != 0 {
 					isTempChanged, isPresChanged, isHumChanged = weatherChanged(weather2, weather1)
 				}
-				printCurrentWeather(city, weather1, isTempChanged, isPresChanged, isHumChanged)
+				showCurrentWeather(city, weather1, isTempChanged, isPresChanged, isHumChanged)
 				monitorCounter++
 			}
 		// exit program
@@ -186,7 +282,6 @@ MonitorWeather:
 			{
 				break MonitorWeather
 			}
-		// make correct choice
 		default:
 			{
 				fmt.Println("Incorrect input!")
